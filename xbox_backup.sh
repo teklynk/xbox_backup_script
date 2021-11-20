340c24df8e7698df01a6f43f3e6fdbd20883ec9a
Ryan Jones <4500737+teklynk@users.noreply.github.com>
2021-11-20 17:24:05 -0500

    Create xbox_backup.sh
    first commit

---
+++ xbox_backup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo "backup started"
+
+xbox_ip="192.168.0.50"
+xbox_ftp_username="xbox"
+xbox_ftp_password="xbox"
+backup_dir="/mnt/BACKUPS/XBOX/"
+backup_log="/home/server1/logs/xbox_backup.log"
+
+truncate -s 0 $backup_log
+
+cd $backup_dir
+
+if [ -d "$xbox_ip/F" ]
+then
+  echo "backup started..."
+  wget -m --user=$xbox_ftp_username --password=$xbox_ftp_password ftp://$xbox_ip/F/games
+  echo "backup of games: completed"
+  #sleep 3
+  #wget -m --user=$xbox_ftp_username --password=$xbox_ftp_password ftp://$xbox_ip/F/roms
+  #echo "backup of roms: completed"
+  #sleep 3
+  #wget -m --user=$xbox_ftp_username --password=$xbox_ftp_password ftp://$xbox_ip/F/homebrew
+  #echo "backup of homebrew: completed"
+  sleep 3
+  wget -m --user=$xbox_ftp_username --password=$xbox_ftp_password ftp://$xbox_ip/E
+  echo "backup of apps: completed"
+  sleep 3
+  echo "Backup completed"
+  exit
+else
+  echo "Directory not found"
+  exit
+fi
